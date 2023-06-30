cb76204c193bc3463301d81c5766c3f8bc5c6d0e
NimoAnthony <nimoanthony25@gmail.com>
2023-07-01 00:24:44 +0800

    LABTRACK_APPLICATION

---
+++ SourceCode/LABTRACK_APPLICATION/labtrack_homepage/android/app/src/main/kotlin/com/example/labtrack_homepage/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.labtrack_homepage
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ SourceCode/LABTRACK_APPLICATION/labtrack_homepage/lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'My App',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.asset(
+              'assets/bsu-logo.png',
+              width: 200,
+              height: 200,
+            ),
+            SizedBox(height: 16),
+            Text(
+              'LABTRACK',
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 5,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
